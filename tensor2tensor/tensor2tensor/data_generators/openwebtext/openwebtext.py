3a7ca3b72ecc7e52718445bcaa2482f1ed62b287
Admin <admin@Edwards-MacBook-Pro.local>
2019-07-16 17:32:11 -0400

    better train_val split

--- tensor2tensor/tensor2tensor/data_generators/openwebtext/openwebtext.py
+++ tensor2tensor/tensor2tensor/data_generators/openwebtext/openwebtext.py
@@ -18,6 +18,23 @@ from tensor2tensor.utils import registry
 
 import tensorflow as tf
 
+split_files = None
+
+def train_dev_split(tmp_dir, split, ratio=0.9):
+  """Split the data into training and validation set."""
+  global split_files
+  if not split_files:
+    dataset_filenames = glob.glob(os.path.join(tmp_dir, '*', '*.txt'))
+    random.shuffle(dataset_filenames)
+    training_num = round(len(dataset_filenames) * 0.9)
+    _train_data_filenames = dataset_filenames[:training_num]
+    _dev_data_filenames = dataset_filenames[training_num:]
+    split_files = {
+        problem.DatasetSplit.TRAIN: _train_data_filenames,
+        problem.DatasetSplit.EVAL: _dev_data_filenames,
+    }
+  return split_files[split]
+
 
 # def _train_data_filenames(tmp_dir):
 #   # return [
@@ -62,22 +79,23 @@ class LanguagemodelOpenWebText(text_problems.Text2SelfProblem):
 
   def generate_samples(self, data_dir, tmp_dir, dataset_split):
     # Get the data filenames and shuffle for train/val split.
-    dataset_filenames = glob.glob(os.path.join(tmp_dir, '*', '*.txt'))
-    random.shuffle(dataset_filenames)
-    training_num = round(len(dataset_filenames) * 0.9)
-    train_data_filenames = dataset_filenames[:training_num]
-    dev_data_filenames = dataset_filenames[training_num:]
-    split_files = {
-        problem.DatasetSplit.TRAIN: train_data_filenames,
-        problem.DatasetSplit.EVAL: dev_data_filenames,
-    }
+    # dataset_filenames = glob.glob(os.path.join(tmp_dir, '*', '*.txt'))
+    # random.shuffle(dataset_filenames)
+    # training_num = round(len(dataset_filenames) * 0.9)
+    # train_data_filenames = dataset_filenames[:training_num]
+    # dev_data_filenames = dataset_filenames[training_num:]
+    # split_files = {
+    #     problem.DatasetSplit.TRAIN: train_data_filenames,
+    #     problem.DatasetSplit.EVAL: dev_data_filenames,
+    # }
     # Need to make sure the data has been downloaded and prepared!
     # _maybe_download_corpus(tmp_dir)
     # original_vocab = _original_vocab(tmp_dir)
 
     # Load the byte_pair_encoder.
     byte_pair_encoder = text_encoder.BytePairEncoder(os.path.join(data_dir, 'encoder.json'), os.path.join(data_dir, 'vocab.bpe'))
-    files = split_files[dataset_split]
+    files = train_dev_split(tmp_dir, dataset_split)
+    # files = split_files[dataset_split]
     for filepath in files:
       tf.logging.info("filepath = %s", filepath)
       for line in tf.gfile.Open(filepath):
