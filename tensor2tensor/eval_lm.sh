9e2af7d04cc78c026178f1de4e0202aabbdbebee
Zhiyu Liang <edward.liang@mail.utoronto.ca>
2019-08-08 15:46:28 -0400

    fixed a typo

--- tensor2tensor/eval_lm.sh
+++ tensor2tensor/eval_lm.sh
@@ -23,7 +23,7 @@ echo "For the last checkpoint, the perplexity = $perplexity" >> $eval_dir/eval_r
 echo "Averaging last 5 checkpoints for every 1k steps..."
 srun --gres=gpu:1 -c 8 --mem=8G -p max12hours python /h/edwardlzy/NLP_Project/tensor2tensor/tensor2tensor/utils/avg_checkpoints.py --checkpoints="$eval_dir/model.ckpt-250000,$eval_dir/model.ckpt-249000,$eval_dir/model.ckpt-248000,$eval_dir/model.ckpt-247000,$eval_dir/model.ckpt-246000" --output_path=$eval_dir/1k_avg.ckpt
 
-echo "Evaluating the average of the last 5 checkpoints..."
+echo "Evaluating the average of the last 5 checkpoints for every 1k steps..."
 srun --gres=gpu:1 -c 8 --mem=16G -p max12hours t2t-eval --data_dir=$data_dir --problem=$problem --model=transformer --hparams_set=$hparams_set --output_dir=$eval_dir --t2t_usr_dir=$usr_dir --hparams="batch_size=1024" --eval_steps=1000 --eval_use_test_set --eval_timeout_mins=0 &> $eval_dir/1k_avg_eval.txt
 loss=`cat $eval_dir/1k_avg_eval.txt | grep -E -o 'loss = [0-9].[0-9]+' | grep -E -o '[0-9].[0-9]+'`
 perplexity=`python -c "import math;print(math.exp($loss))"`
@@ -34,7 +34,7 @@ echo "For the average of every 1k checkpoints, the perplexity = $perplexity" >> 
 echo "Averaging last 5 checkpoints for every 5k steps..."
 srun --gres=gpu:1 -c 8 --mem=8G -p max12hours python /h/edwardlzy/NLP_Project/tensor2tensor/tensor2tensor/utils/avg_checkpoints.py --checkpoints="$eval_dir/model.ckpt-250000,$eval_dir/model.ckpt-245000,$eval_dir/model.ckpt-240000,$eval_dir/model.ckpt-235000,$eval_dir/model.ckpt-231000" --output_path=$eval_dir/5k_avg.ckpt
 
-echo "Evaluating the average of the last 5 checkpoints..."
+echo "Evaluating the average of the last 5 checkpoints for every 5k steps..."
 srun --gres=gpu:1 -c 8 --mem=16G -p max12hours t2t-eval --data_dir=$data_dir --problem=$problem --model=transformer --hparams_set=$hparams_set --output_dir=$eval_dir --t2t_usr_dir=$usr_dir --hparams="batch_size=1024" --eval_steps=1000 --eval_use_test_set --eval_timeout_mins=0 &> $eval_dir/5k_avg_eval.txt
 loss=`cat $eval_dir/5k_avg_eval.txt | grep -E -o 'loss = [0-9].[0-9]+' | grep -E -o '[0-9].[0-9]+'`
 perplexity=`python -c "import math;print(math.exp($loss))"`
@@ -45,7 +45,7 @@ echo "For the average of every 5k checkpoints, the perplexity = $perplexity" >> 
 echo "Averaging last 10 checkpoints for every 1k steps..."
 srun --gres=gpu:1 -c 8 --mem=8G -p max12hours python /h/edwardlzy/NLP_Project/tensor2tensor/tensor2tensor/utils/avg_checkpoints.py --checkpoints="$eval_dir/model.ckpt-250000,$eval_dir/model.ckpt-249000,$eval_dir/model.ckpt-248000,$eval_dir/model.ckpt-247000,$eval_dir/model.ckpt-246000,$eval_dir/model.ckpt-245000,$eval_dir/model.ckpt-244000,$eval_dir/model.ckpt-243000,$eval_dir/model.ckpt-242000,$eval_dir/model.ckpt-241000" --output_path=$eval_dir/1k_10_avg.ckpt
 
-echo "Evaluating the average of the last 5 checkpoints..."
+echo "Evaluating the average of the last 10 checkpoints..."
 srun --gres=gpu:1 -c 8 --mem=16G -p max12hours t2t-eval --data_dir=$data_dir --problem=$problem --model=transformer --hparams_set=$hparams_set --output_dir=$eval_dir --t2t_usr_dir=$usr_dir --hparams="batch_size=1024" --eval_steps=1000 --eval_use_test_set --eval_timeout_mins=0 &> $eval_dir/1k_10_avg_eval.txt
 loss=`cat $eval_dir/1k_10_avg_eval.txt | grep -E -o 'loss = [0-9].[0-9]+' | grep -E -o '[0-9].[0-9]+'`
 perplexity=`python -c "import math;print(math.exp($loss))"`
